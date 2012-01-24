6fce2876a08c6819224859084d4f7578b4b27ae9
werner mendizabal <nonameentername@gmail.com>
2012-01-23 23:21:03 -0600

    update ndk_module_path

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ export NDKPLATFORM="$NDK/platforms/android-9/arch-arm"
 
 export PATH="$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/:$NDK:$SDK/tools:$PATH"
 
-NDK_MODULE_PATH=$HOME/source/irrlicht-android/project/jni ndk-build
+NDK_MODULE_PATH=$ROOTDIR/../irrlicht-android/project/jni ndk-build
 
 mkdir -p $ROOTDIR/irrlicht/bin
 
